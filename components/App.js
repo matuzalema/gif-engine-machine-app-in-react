4b91fb59078e5a05f59273031d58415db1ecb06f
Jadwiga Bochinska <matuzalema@gmail.com>
2019-02-21 12:35:56 +0100

    change App.js

--- components/App.js
+++ components/App.js
@@ -48,7 +48,7 @@ App = React.createClass({
       	return (
         	<div style={styles}>
 	            <h1>Wyszukiwarka GIFow!</h1>
-	            <p>Znajdź gifa na <a href={'http://giphy.com'}>giphy</a> Naciskaj enter, aby pobrać kolejne gify'.</p>
+	            <p>Znajdź gifa na <a href={'http://giphy.com'}>giphy</a> Naciskaj enter, aby pobrać kolejne gify</p>
 	            <Search onSearch={this.handleSearch}/>
 	            <Gif 
 	                loading={this.state.loading}
